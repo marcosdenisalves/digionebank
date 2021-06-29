5dc8b14e756e7db8ad9984172baaf8d3a6ba0835
Denis Alves <dee.alves2@gmail.com>
2021-06-28 22:16:25 -0300

    Primeiros conceitos com set

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,17 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Dênis"
     var cpf: String = "123.456.789-10"
-
-    inner class Endereco {
-        var rua: String = "Rua 17 de Abril"
-    }
+    private set
 }
 
 fun main() {
     val denis = Pessoa()
 
+    println(denis)
     println(denis.nome)
     println(denis.cpf)
-
-    println(denis.Endereco().rua)
 }
