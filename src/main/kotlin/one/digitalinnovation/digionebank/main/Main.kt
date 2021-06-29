75f0832456d69b62b2f942a2df23515e1399a4ce
Denis Alves <dee.alves2@gmail.com>
2021-06-28 22:29:00 -0300

    Modularizando aplicação

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/main/Main.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank.main
+
+import one.digitalinnovation.digionebank.models.Pessoa
+
+fun main() {
+    val denis = Pessoa()
+    println(denis.nome)
+}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/models/Pessoa.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digionebank.models
+
+class Pessoa {
+    var nome: String = "Dênis"
+    var cpf: String = "123.456.789-10"
+        private set
+}
